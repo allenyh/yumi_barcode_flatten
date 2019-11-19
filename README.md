fc4de33cb3f4524efbd35fe7cf4e9e60f176fc30
allenyh <bicycleboy323@gmail.com>
2019-11-19 12:36:26 +0800

    first commit.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # yumi_barcode_flatten
+The repo is prepared for YUMI yumi_barcode_flatten project.
+
+## Prerequisite
+ 1. Ubuntu 16.04/18.04 with Nvidia driver >= 384.xx (supprt cuda9.0 or newer)
+ 2. [Docker](https://docs.docker.com/install/linux/docker-ce/ubuntu/) & [nvidia-docker2](https://github.com/NVIDIA/nvidia-docker#upgrading-with-nvidia-docker2-deprecated)
+
+## Setup 
+a. Clone repo in $HOME directory
+```bash
+    $ cd ~/ && git clone https://github.com/allenyh/yumi_barcode_flatten.git
+    $ git submodule init && git submodule update
+```
+
+b. Docker run 
+```bash
+    # It still can work for ROS stuff if your laptop doesn't support cuda10.
+    $ docker pull argnctu/yumi-project:cuda10.0    # To check your docker image is latest version
+    $ cd yumi_barcode_flatten
+    $ source docker_run.sh [cuda10 | same]    # tag"same" is for "docker exec" command
+```
+
+c. Compile the code **in docker container**
+```bash
+    $ cd ~/yumi_barcode_flatten/catkin_ws
+    $ catkin_make
+```
+
+___
+
+## How to run
+### TODO Write
+
+
+
+
+
+
---
+++ docker_run.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+xhost +local:docker
+
+# New docker user
+DOCKER_USER="developer"
+
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_NC='\033[0m'
+
+#
+# Check the command 'nvidia-docker' existed or not
+#
+ret_code="$(command -v nvidia-docker)"
+if [ -z "$ret_code" ]; then
+    DOCKER_CMD="docker"
+else
+    DOCKER_CMD="nvidia-docker"
+fi
+
+
+#
+# Specify cuda version
+#
+if [ $# -gt 0 ]; then
+    if [[ "$1" == "cuda9" || "$1" == "cuda9.0" ]] ; then
+        echo -e "RUN: \"${DOCKER_CMD}\""
+        DOCKER_TAG="cuda9.0"
+    elif [[ "$1" == "cuda10" || "$1" == "cuda10.0" ]] ; then
+        echo -e "RUN: \"${DOCKER_CMD}\""
+        DOCKER_TAG="cuda10.0"
+    elif [ "$1" == "same" ] ; then
+        echo -e "RUN: \"docker exec\""
+    else
+        echo -e "Please specify which cuda version your GPU support."
+        echo -e "${COLOR_RED}Usage: source docker_run.sh [cuda9 | cuda10 | same]${COLOR_NC}"
+    fi
+else
+    echo -e "${COLOR_RED}Usage: source docker_run.sh [cuda9 | cuda10| same]${COLOR_NC}"
+fi
+
+
+#
+# Execute command
+#
+if [ $# -gt 0 ]; then
+    if [ "$1" == "same" ]; then
+        docker exec -it yumi-barcode-flatten bash
+    else
+        ${DOCKER_CMD} run --name yumi-barcode-flatten --rm -it --net=host --privileged -v /dev:/dev \
+            -e DISPLAY=$DISPLAY \
+            -v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock \
+            -v /home/$USER/yumi_barcode_flatten:/home/${DOCKER_USER}/yumi_barcode_flatten \
+            -v /tmp/.X11-unix/:/tmp/.X11-uni \
+            -w /home/${DOCKER_USER}/yumi_barcode_flatten \
+            --device=/dev/dri:/dev/dri \
+            --device=/dev/nvhost-ctrl \
+            --device=/dev/nvhost-ctrl-gpu \
+            --device=/dev/nvhost-prof-gpu \
+            --device=/dev/nvmap \
+            --device=/dev/nvhost-gpu \
+            --device=/dev/nvhost-as-gpu \
+            --device=/dev/ttyUSB0 \
+            -v /dev/bus/usb:/dev/bus/usb \
+            argnctu/yumi-project:${DOCKER_TAG}
+    fi
+else
+    echo "please provide docker tag name."
+fi
